6b6f6ce99ce4f6fe07887cc2e75b46f77114d578
Roman Bugaian <bugaian.roman@gmail.com>
2023-03-06 22:37:35 +0100

    Initial Commit

---
+++ Beamer/BeamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeamerApp.swift
+//  Beamer
+//
+//  Created by Roman Bugaian on 06.03.23.
+//
+
+import SwiftUI
+
+@main
+struct BeamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
